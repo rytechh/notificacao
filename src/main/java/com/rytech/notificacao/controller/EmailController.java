ce4b5dd4d24aea894eecb7b01ed80458d8341d54
Raian <raianros15@gmail.com>
2026-03-21 20:28:54 -0300

    adicionados métodos de envio de email

---
+++ src/main/java/com/rytech/notificacao/controller/EmailController.java
@@ -0,0 +1,26 @@
+package com.rytech.notificacao.controller;
+
+import com.rytech.notificacao.business.EmailService;
+import com.rytech.notificacao.business.dto.TarefasDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/email")
+public class EmailController {
+
+    private final EmailService emailService;
+
+    @PostMapping
+    public ResponseEntity<Void> enviarEmail(@RequestBody TarefasDTO dto) {
+        emailService.enviaEmail(dto);
+        return ResponseEntity.ok().build();
+
+    }
+
+}
